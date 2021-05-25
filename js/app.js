202e9bc56338167f89430811152d90dac2e27eaa
AbdulsalamNAJ <abd.alsalam93@gmail.com>
2021-05-25 08:11:40 +0300

    working on index.html

---
+++ js/app.js
@@ -0,0 +1,7 @@
+'use strict'
+
+let favFood = prompt('Is spaghetti my best meal?');
+let favSport = prompt('Do I like to play basketball?');
+let favPet = prompt('Is it better for me to have dogs than cats?');
+let favMovie = prompt('Is it better for me to stay at home and enjoy watching action movies rather than romantic movies?');
+let favCar = prompt('Do I prefer Mercedes cars over BMW cars?')
