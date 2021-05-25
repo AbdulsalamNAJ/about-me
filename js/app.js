18b55c5693d280aa6da35e3712fa263c8c5122d9
AbdulsalamNAJ <abd.alsalam93@gmail.com>
2021-05-25 15:10:20 +0300

    add button to HTML page

--- js/app.js
+++ js/app.js
@@ -1,88 +1,89 @@
-'use strict'
+'use strict';
 
-let i = "";
-let msg = "";
+let i = '';
+let msg = '';
+let myBtn = document.getElementById('myBtn');
+myBtn.addEventListener('click',myFun);
 
 function myFun(){
 
-alert('Dear visitor, I would like to play a guess game with :D');
-let favFood = prompt('Is spaghetti my best meal?   If yes enter  [ yes ]  or  [ y ]');
+  alert('Dear visitor, I would like to play a guess game with :D');
+  let favFood = prompt('Is spaghetti my best meal?   If yes enter  [ yes ]  or  [ y ]');
 
-if (favFood == null){
-    favFood = "";
-}else {
+  if (favFood === null){
+    favFood = '';
+  }else {
     favFood = favFood.toLowerCase();
-}
-if (favFood == 'y' || favFood == 'yes' ){
+  }
+  if (favFood === 'y' || favFood === 'yes' ){
     i += 'T';
-}else {
+  }else {
     i += 'F';
-}
-console.log(i + favFood);
+  }
+  console.log(i + favFood);
 
-let favSport = prompt('Do I like to play basketball?   If yes enter  [ yes ]  or  [ y ]');
+  let favSport = prompt('Do I like to play basketball?   If yes enter  [ yes ]  or  [ y ]');
 
-if (favSport == null){
-    favSport = "";
-}else {
+  if (favSport === null){
+    favSport = '';
+  }else {
     favSport = favSport.toLowerCase();
-}
-if (favSport == 'y' || favSport == 'yes' ){
+  }
+  if (favSport === 'y' || favSport === 'yes' ){
     i += 'T';
-}else {
+  }else {
     i += 'F';
-}
-console.log(i + favSport);
+  }
+  console.log(i + favSport);
 
-let favPet = prompt('Is it better for me to have dogs than cats?   If yes enter  [ yes ]  or  [ y ]');
+  let favPet = prompt('Is it better for me to have dogs than cats?   If yes enter  [ yes ]  or  [ y ]');
 
-if (favPet == null){
-    favPet = "";
-}else {
+  if (favPet === null){
+    favPet = '';
+  }else {
     favPet = favPet.toLowerCase();
-}
-if (favPet == 'y' || favPet == 'yes' ){
+  }
+  if (favPet === 'y' || favPet === 'yes' ){
     i += 'T';
-}else {
+  }else {
     i += 'F';
-}
-console.log(i + favFood);
+  }
+  console.log(i + favFood);
 
-let favMovie = prompt('Is it better for me to stay at home and enjoy watching action movies rather than romantic movies?   If yes enter  [ yes ]  or  [ y ]');
+  let favMovie = prompt('Is it better for me to stay at home and enjoy watching action movies rather than romantic movies?   If yes enter  [ yes ]  or  [ y ]');
 
-if (favMovie == null){
-    favMovie = "";
-}else {
+  if (favMovie === null){
+    favMovie = '';
+  }else {
     favMovie = favMovie.toLowerCase();
-}
-if (favMovie == 'y' || favMovie == 'yes' ){
+  }
+  if (favMovie === 'y' || favMovie === 'yes' ){
     i += 'T';
-}else {
+  }else {
     i += 'F';
-}
-console.log(i + favMovie);
+  }
+  console.log(i + favMovie);
 
-let favCar = prompt('Do I prefer Mercedes cars over BMW cars?   If yes enter  [ yes ]  or  [ y ]');
+  let favCar = prompt('Do I prefer Mercedes cars over BMW cars?   If yes enter  [ yes ]  or  [ y ]');
 
-if (favCar == null){
-    favCar = "";
-}else {
+  if (favCar === null){
+    favCar = '';
+  }else {
     favCar = favCar.toLowerCase();
-}
-if (favCar == 'y' || favCar == 'yes' ){
+  }
+  if (favCar === 'y' || favCar === 'yes' ){
     i += 'T';
-}else {
+  }else {
     i += 'F';
-}
-console.log(i + favCar);
+  }
+  console.log(i + favCar);
 
-for (let x = 0; x <= 4; x++){
-    if (i[x] == 'T'){
-        msg += "True! ";       
+  for (let x = 0; x <= 4; x++){
+    if (i[x] === 'T'){
+      msg += 'True! ';
     }else {
-        msg += "False! ";
+      msg += 'False! ';
     }
-}
-
-alert(msg);
+  }
+  alert(msg);
 }
