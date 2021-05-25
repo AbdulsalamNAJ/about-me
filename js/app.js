992ce11159ee361584e2f5436c1301093bbbdefa
AbdulsalamNAJ <abd.alsalam93@gmail.com>
2021-05-25 08:45:41 +0300

    writing questions in JS file

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict'
 
 let i = "";
-let favFood = prompt('Is spaghetti my best meal?');
+let favFood = prompt('Is spaghetti my best meal?   If yes enter  [ yes ]  or  [ y ]');
 
 if (favFood == null) {
     favFood = "";
@@ -14,55 +14,59 @@ if (favFood == 'y' || favFood == 'yes' ) {
     i += 'F';
 }
 console.log(i + favFood);
-let favSport = prompt('Do I like to play basketball?');
 
-if (favFood == null) {
-    favFood = "";
+let favSport = prompt('Do I like to play basketball?   If yes enter  [ yes ]  or  [ y ]');
+
+if (favSport == null) {
+    favSport = "";
 }else {
-    favFood = favFood.toLowerCase();
+    favSport = favSport.toLowerCase();
 }
-if (favFood == 'y' || favFood == 'yes' ) {
+if (favSport == 'y' || favSport == 'yes' ) {
+    i += 'T';
+}else {
+    i += 'F';
+}
+console.log(i + favSport);
+
+let favPet = prompt('Is it better for me to have dogs than cats?   If yes enter  [ yes ]  or  [ y ]');
+
+if (favPet == null) {
+    favPet = "";
+}else {
+    favPet = favPet.toLowerCase();
+}
+if (favPet == 'y' || favPet == 'yes' ) {
     i += 'T';
 }else {
     i += 'F';
 }
 console.log(i + favFood);
-let favPet = prompt('Is it better for me to have dogs than cats?');
 
-if (favFood == null) {
-    favFood = "";
+let favMovie = prompt('Is it better for me to stay at home and enjoy watching action movies rather than romantic movies?   If yes enter  [ yes ]  or  [ y ]');
+
+if (favMovie == null) {
+    favMovie = "";
 }else {
-    favFood = favFood.toLowerCase();
+    favMovie = favMovie.toLowerCase();
 }
-if (favFood == 'y' || favFood == 'yes' ) {
+if (favMovie == 'y' || favMovie == 'yes' ) {
     i += 'T';
 }else {
     i += 'F';
 }
-console.log(i + favFood);
-let favMovie = prompt('Is it better for me to stay at home and enjoy watching action movies rather than romantic movies?');
+console.log(i + favMovie);
 
-if (favFood == null) {
-    favFood = "";
+let favCar = prompt('Do I prefer Mercedes cars over BMW cars?   If yes enter  [ yes ]  or  [ y ]');
+
+if (favCar == null) {
+    favCar = "";
 }else {
-    favFood = favFood.toLowerCase();
+    favCar = favCar.toLowerCase();
 }
-if (favFood == 'y' || favFood == 'yes' ) {
+if (favCar == 'y' || favCar == 'yes' ) {
     i += 'T';
 }else {
     i += 'F';
 }
-console.log(i + favFood);
-let favCar = prompt('Do I prefer Mercedes cars over BMW cars?');
-
-if (favFood == null) {
-    favFood = "";
-}else {
-    favFood = favFood.toLowerCase();
-}
-if (favFood == 'y' || favFood == 'yes' ) {
-    i += 'T';
-}else {
-    i += 'F';
-}
-console.log(i + favFood);
+console.log(i + favCar);
