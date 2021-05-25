24531e9e0a3309714cb37e12e2dd8a98434e2641
AbdulsalamNAJ <abd.alsalam93@gmail.com>
2021-05-25 08:38:06 +0300

    adding questions with console.log in JS file

--- js/app.js
+++ js/app.js
@@ -1,7 +1,68 @@
 'use strict'
 
+let i = "";
 let favFood = prompt('Is spaghetti my best meal?');
+
+if (favFood == null) {
+    favFood = "";
+}else {
+    favFood = favFood.toLowerCase();
+}
+if (favFood == 'y' || favFood == 'yes' ) {
+    i += 'T';
+}else {
+    i += 'F';
+}
+console.log(i + favFood);
 let favSport = prompt('Do I like to play basketball?');
+
+if (favFood == null) {
+    favFood = "";
+}else {
+    favFood = favFood.toLowerCase();
+}
+if (favFood == 'y' || favFood == 'yes' ) {
+    i += 'T';
+}else {
+    i += 'F';
+}
+console.log(i + favFood);
 let favPet = prompt('Is it better for me to have dogs than cats?');
+
+if (favFood == null) {
+    favFood = "";
+}else {
+    favFood = favFood.toLowerCase();
+}
+if (favFood == 'y' || favFood == 'yes' ) {
+    i += 'T';
+}else {
+    i += 'F';
+}
+console.log(i + favFood);
 let favMovie = prompt('Is it better for me to stay at home and enjoy watching action movies rather than romantic movies?');
-let favCar = prompt('Do I prefer Mercedes cars over BMW cars?')
+
+if (favFood == null) {
+    favFood = "";
+}else {
+    favFood = favFood.toLowerCase();
+}
+if (favFood == 'y' || favFood == 'yes' ) {
+    i += 'T';
+}else {
+    i += 'F';
+}
+console.log(i + favFood);
+let favCar = prompt('Do I prefer Mercedes cars over BMW cars?');
+
+if (favFood == null) {
+    favFood = "";
+}else {
+    favFood = favFood.toLowerCase();
+}
+if (favFood == 'y' || favFood == 'yes' ) {
+    i += 'T';
+}else {
+    i += 'F';
+}
+console.log(i + favFood);
